8b0eadf2f0ed80ea8527b28c078ef8c4914d54c1
lin-zhao-cheng <jjaacckk41208@gmail.com>
2024-03-13 23:39:24 +0800

    EP5 v-if、v-else、v-show

--- src/components/componentA.vue
+++ src/components/componentA.vue
@@ -2,17 +2,31 @@
 import { ref, onMounted } from 'vue'
 const data="Hi Vue 351454"
 const text = ref("123")
+const view = ref(1)
+const changeView=(index)=>{
+    view.value = index
+}
+// const changeView2=()=>{
+//     view.value = 1
+// }
 </script>
 
 
 <template>
+    <button v-on:click="changeView(1)">顯示單向綁定</button>
+    <button @click="changeView(2)">顯示雙向綁定</button>
+    <button @click="changeView(333)">沒畫面</button>
+<div class="container01" v-if="view===1">
+    <p> 資料單向綁定</p>
+    {{ data }}
+</div>
 
-<p> 單向綁定</p>
-{{ data }}
-
-<p> 雙向綁定</p>
-<input type="text" v-model="text"/>
-<p>{{ text }}</p>
+<div class="container02" v-if="view===2">
+    <p> 資料雙向綁定</p>
+    <input type="text" v-model="text"/>
+    <p>{{ text }}</p>
+</div>
+<div v-else>沒畫面</div>
 
 </template>
 
@@ -21,5 +35,10 @@ const text = ref("123")
 
 
 <style scoped>
-
+.container01{
+    border: 2px red solid;
+}
+.container02{
+    border: 2px blue solid;
+}
 </style>
