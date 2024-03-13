0ab3bfb2063010bed2b4851f4607765ad7b44a21
lin-zhao-cheng <jjaacckk41208@gmail.com>
2024-03-13 23:14:57 +0800

    EP4_單雙向綁定、v-model、ref

---
+++ src/components/componentA.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+const data="Hi Vue 351454"
+const text = ref("123")
+</script>
+
+
+<template>
+
+<p> 單向綁定</p>
+{{ data }}
+
+<p> 雙向綁定</p>
+<input type="text" v-model="text"/>
+<p>{{ text }}</p>
+
+</template>
+
+
+
+
+
+<style scoped>
+
+</style>
