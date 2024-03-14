1266512b8c989503719f725d573114a3f1fb8ee9
lin-zhao-cheng <jjaacckk41208@gmail.com>
2024-03-14 20:55:57 +0800

    不知道，早上做的

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import componentA from './components/componentA.vue';
 import componentB from './components/componentB.vue';
 import componentC from './components/componentC.vue';
+import componentD from './components/componentD.vue';
 </script>
 
 
 <template>
-<componentC/>
+<componentD/>
 </template>
 
 
---
+++ src/components/componentD.vue
@@ -0,0 +1,19 @@
+<script >
+import {ref, createApp } from 'vue';
+
+const vm = createApp({
+setup(){
+    const message = ref('Hello vue 3.0');
+    return{
+        message
+    }
+}
+});
+vm.mount('#app');
+</script>
+<template>
+<div id="app">
+  {{ message }}
+</div>
+</template>
+<style scoped></style>
